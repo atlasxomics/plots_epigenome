ec231ed22ac01550b5b5472de80be1ec096c077f
jpmcga <jpaulmcgann@gmail.com>
2025-07-07 13:43:34 -0400

    remove unnecessary parameters from track browser

--- track_browser/igv.py
+++ track_browser/igv.py
@@ -19,59 +19,11 @@ coverages_group = w_select(
     appearance={
       "help_text": "Select grouping for coverage tracks."
     }
+    
 )
-
-coverages_gene = w_select(
-    label="Choose a gene to view",
-    options=available_genes,
-    appearance={
-      "help_text": "Specify gene for coverage tracks."
-    }
-)
-
-coverages_min = w_text_input(
-    label="Track min (optional)",
-    default="",
-    appearance={
-      "help_text": "Fix y-axis min for tracks"
-    }
-)
-
-coverages_max = w_text_input(
-    label="Track max (optional)",
-    default="",
-    appearance={
-      "help_text": "Fix y-axis max for tracks"
-    }
-)
-
 coverages_button = w_button(label="Update IGV Viewer")
 
-input_row = w_row(items=[coverages_gene, coverages_min, coverages_max])
-input_column = w_column(items=[coverages_group, input_row, coverages_button])
-
-if coverages_min.value != "":
-  try:
-    float(coverages_min.value)
-  except ValueError:
-    w_text_output(
-      content="Could not convert 'Track min' to float; please input a valid number.",
-      appearance={"message_box": "warning"}
-    )
-    submit_widget_state()
-
-if coverages_max.value != "":
-  try:
-    float(coverages_max.value)
-  except ValueError:
-    w_text_output(
-      content="Could not convert 'Track max' to float; please input a valid number.",
-      appearance={"message_box": "warning"}
-    )
-    submit_widget_state()
-
-if coverages_gene.value is not None and coverages_group.value is not None and coverages_button.value:
-    print(coverages_min.value, coverages_max.value)
+if coverages_group.value is not None and coverages_button.value:
 
     coverages_dir = coverages_dict[coverages_group.value]
 
@@ -112,7 +64,6 @@ if coverages_gene.value is not None and coverages_group.value is not None and co
         files = bigwigs
 
     tracks = []
-    count = 0
     for f in files:
         tracks.append({
             "name": f.path.split("/")[-1],
@@ -122,27 +73,9 @@ if coverages_gene.value is not None and coverages_group.value is not None and co
             "autoscale": False,
             "visibilityWindow": 100000,
         })
-        if coverages_min.value != "":
-          try:
-            tracks[count]["min"] = float(coverages_min.value)
-          except:
-            print("Can't convert min")
-            continue
-        if coverages_max.value != "":
-          try:
-            tracks[count]["max"] = float(coverages_max.value)
-          except:
-            print("Can't convert max")
-            continue
-
-        count += 1
-    print(tracks)
-
-    selected_gene = coverages_gene.value
 
     opts: IGVOptions = {
         "genome": "hg38",
-        "locus": selected_gene,
         "tracks": tracks,
     }
 
