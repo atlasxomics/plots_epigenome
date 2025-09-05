022c680beb9d967e6117abd80058fc707393bba2
jpmcga <jpaulmcgann@gmail.com>
2025-09-05 13:35:25 -0400

    Add docs, add box/violin option

--- violin_plots/violin_plot.py
+++ violin_plots/violin_plot.py
@@ -2,10 +2,11 @@ w_text_output(content="""
 
 # Violin Plot
 
-<details>
-<summary><i>details</i></summary>
-For a provided grouping (Clusters, Samples, Conditions), display a violin plot showing the distribution for numeric cell data, gene accessibility, or motif deviation data.
-</details>
+Generate violin plots to visualize the distribution of values across a selected grouping (e.g., **Clusters**, **Samples**, or **Conditions**).  
+You can display numeric cell-level metrics, gene accessibility values, or motif deviation scores.
+
+> If both gene and motif `AnnData` objects are available, the data dropdown will include names from both.  
+> Motif names are distinguished by a numeric suffix (e.g., `CTCF-177`).
 
 """)
 
@@ -31,9 +32,11 @@ violin_data = w_select(
   default="tsse",
   options=tuple(numeric_metadata + available_motifs + available_genes),
   appearance={
-    "help_text": "Select values to plot."
+    "help_text": "Select values to plot.",
+    "description": "Motifs are denoted with a numberic suffix ie. CTCT-177."
   }
 )
+
 violin_group_by = w_select(
   label="group",
   default="cluster",
@@ -41,6 +44,16 @@ violin_group_by = w_select(
   appearance={
     "detail": "(cluster, sample, condition)",
     "help_text": "Select group to display on x-axis."
+  }
+)
+
+violin_type = w_select(
+  label="plot type",
+  default="box",
+  options=tuple(["box", "violin"]),
+  appearance={
+    "detail": "(box, violin)",
+    "help_text": "Use box to speed up large datasets."
   }
 )
 
@@ -61,27 +74,45 @@ if not data_type:
   exit()
 
 
-w_row(items=[violin_data, violin_group_by])
+w_row(items=[violin_data, violin_group_by, violin_type])
 
-v_button = w_button(label="Update Violin Plot")
-
-if v_button.value and data_type is not None:
+if data_type is not None:
 
   v_adata = adata_m if data_type == "motif" else adata_g
   
   violin_df = create_violin_data(
     v_adata, violin_group_by.value, violin_data.value, data_type=data_type
   )
-  
-  violin_fig = px.violin(
-      violin_df,
-      x='group',
-      y='value',
-      box=True,
-      points=False,
-      color='group',
-      color_discrete_sequence=px.colors.qualitative.Alphabet,
-  )
+
+  if violin_type.value == "box":
+    violin_fig = px.box(
+        violin_df,
+        x='group',
+        y='value',
+        points=False,
+        color='group',
+        color_discrete_sequence=px.colors.qualitative.Alphabet,
+    )
+
+  elif violin_type.value == "violin":
+    violin_fig = px.violin(
+        violin_df,
+        x='group',
+        y='value',
+        box=True,
+        points=False,
+        color='group',
+        color_discrete_sequence=px.colors.qualitative.Alphabet,
+      )
+
+  else:
+    w_text_output(
+      content="Plot type not recognized",
+      appearance={"message_box": "warning"}
+    )
+    submit_widget_state()
+    exit()
+
   violin_fig.update_layout(
       title=f"Distribution of {violin_data.value} by {violin_group_by.value}",
       xaxis_title=violin_group_by.value,
@@ -94,3 +125,6 @@ if v_button.value and data_type is not None:
 
   w_plot(source=violin_fig)
 
+else:
+  w_output_text(content="  ")
+  sumbit_widget_state()
