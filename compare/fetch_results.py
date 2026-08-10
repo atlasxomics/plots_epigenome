10ef74bd6c681d4565ae19f298a799d445653950
jpmcga <jpaulmcgann@gmail.com>
2026-08-10 14:07:03 -0400

    add mm39 options

--- compare/fetch_results.py
+++ compare/fetch_results.py
@@ -176,7 +176,7 @@ else:
     label="Reference Geneome",
     default=None,
     key="compare_genome",
-    options=tuple(["hg38", "mm10", "rn6"]),
+    options=("hg38", "mm10", "mm39", "rn6"),
     appearance={
       "help_text": "Reference genome for experiment."
     }
